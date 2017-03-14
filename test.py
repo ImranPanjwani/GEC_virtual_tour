739ed75f17673c2cb18cc53af808ced56258aadf
Imran Panjwani <imranpanjwani00@gmail.com>
2017-03-14 22:52:12 +0530

    change file structure

--- test.py
+++ test.py
@@ -9,9 +9,9 @@ if __name__ == '__main__':
              
              'tools.staticdir.on': True,
              'tools.staticdir.root': os.path.abspath(os.getcwd()),
-             'tools.staticdir.dir': 'images',
+             'tools.staticdir.dir': '/home/imran/Desktop/gec_vr/GEC_virtual_tour/static',
              
          }
              }
-cherrypy.config.update({'server.socket_port': 8080})
+cherrypy.config.update({'server.socket_port': 8085})
 cherrypy.quickstart(MyApp(),"/",conf)
